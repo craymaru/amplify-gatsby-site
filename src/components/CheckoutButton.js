48ae5ddde87aa9cffbb0957c5a41d918fc16d1d0
Cray <craymaru@gmail.com>
2022-08-12 20:33:27 +0900

    feat: Add CheckoutButton

---
+++ src/components/CheckoutButton.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const CheckoutButton = () => {
+  const redirectToCheckout = async () => {}
+
+  return <button onClick={redirectToCheckout}>Continue to payment</button>
+}
+
+export default CheckoutButton
