6c53c99c01d747bfc6934c1b2613f8527e165381
Cray <craymaru@gmail.com>
2022-08-13 00:51:15 +0900

    feat: Add amplify config

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,9 @@
 import "@stripe/stripe-js"
+import Amplify, { Auth } from "aws-amplify"
+import awsconfig from "./src/aws-exports"
+
+Amplify.configure(awsconfig)
+Auth.configure(awsconfig)
 
 /**
  * Implement Gatsby's Browser APIs in this file.
