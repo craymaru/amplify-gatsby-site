052c9a565d6e269284bd4659a9b8ae660b882694
Cray <craymaru@gmail.com>
2022-08-12 20:10:22 +0900

    feat: Add stripe API function

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "stripeApi": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
